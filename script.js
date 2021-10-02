8aa79fc74c51f9d278fc800f49cc483ea311a9fd
krifty <kaitlynmariejones@gmail.com>
2021-10-02 12:13:29 -0700

    check for winner

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let gameStatus = (function () {
     //player objects
     let player = {
-        create: function(name, symbol, score) {
+        create: function (name, symbol, score) {
             let instance = Object.create(this);
             instance.name = name;
             instance.symbol = symbol;
@@ -9,7 +9,7 @@ let gameStatus = (function () {
             return instance;
         }
     }
-    
+
 
     let player1 = player.create("Player 1", "X", 0)
     let player2 = player.create("Player 2", "O", 0)
@@ -80,22 +80,23 @@ let gameBoard = (function () {
         changeSquareDisplay(this, player);
         hoverToggle(this);
     }
-    
+
     function unhover() {
         hoverToggle(this);
     }
-    
+
     function hoverToggle(element) {
         element.classList.toggle("unhover")
         element.classList.toggle("hover")
     }
-    
+
     function click() {
         let eClassList = this.classList
-        
+
         if (eClassList.contains("validTurn")) {
             eClassList.add('played');
             eClassList.remove("validTurn");
+            winCheck();
             pubsubs.emit('validTurn', true);
             this.removeEventListener("mouseover", hover);
         }
@@ -120,4 +121,112 @@ let gameBoard = (function () {
             square.addEventListener("mouseout", unhover);
         });
     }
+
+    function winCheck() {
+        let cubeContainer = []
+        let cube = {
+            create: function (name, validIndicator, playerMark, beenPlayed) {
+                var instance = Object.create(this);
+                instance.name = name;
+                instance.validIndicator = validIndicator;
+                instance.playerMark = playerMark;
+                instance.beenPlayed = beenPlayed;
+                return instance;
+            }
+        }
+
+        for (let i = 0; i < square.length; i++) {
+            let cubeObject = cube.create(i, !(square[i].classList.contains("validTurn")), (square[i].innerHTML), (square[i].classList.contains("played")));
+            cubeContainer.push(cubeObject);
+        }
+
+        function squareToCheck(num) {
+            return (cubeContainer[num].validIndicator && cubeContainer[num].playerMark == player);
+        }
+
+        //1st square/1row 
+        if (squareToCheck(0)) {
+           //straight across
+            if (squareToCheck(1)) {
+                if (squareToCheck(2)) {
+                   gameOver("victory");
+                    return;
+                }
+            }  
+            // down
+            if (squareToCheck(3)) {
+                if (squareToCheck(6)) {
+                   gameOver("victory");
+                    return;
+                }
+            } 
+            // diagonal
+            if (squareToCheck(4)) {
+                if (squareToCheck(8)) {
+                   gameOver("victory");
+                    return;
+                }
+            }
+        } 
+        //2nd square, 1st Row down
+        if (squareToCheck(1)) {
+            if (squareToCheck(4)) {
+                if (squareToCheck(7)) {
+                   gameOver("victory");
+                    return;
+                }
+            }
+        } 
+        //3rd square/1st 
+        if (squareToCheck(2)) {
+            // down
+            if (squareToCheck(5)) {
+                if (squareToCheck(8)) {
+                   gameOver("victory");
+                    return;
+                }
+            }
+             // diagonal
+            if (squareToCheck(4)) {
+                if (squareToCheck(6)) {
+                   gameOver("victory");
+                    return;
+                }
+            }
+        } 
+        //2nd row across 
+        if (squareToCheck(3)) {
+            if (squareToCheck(4)) {
+                if (squareToCheck(5)) {
+                   gameOver("victory");
+                    return;
+                }
+            }
+        } 
+        //third row across
+        if (squareToCheck(6)) {
+            if (squareToCheck(7)) {
+                if (squareToCheck(8)) {
+                    gameOver("victory");
+                    return;
+                }
+            }
+        }
+        //tie
+        if (cubeContainer.every((eachItem) => eachItem.validIndicator)) {
+            gameOver("TIE!!!!");
+        }
+    }
+
+    function gameOver(result) {
+        if (result == "TIE!!!!") {
+            alert("tie");
+        }else if (result == "victory") {
+            alert("'victory");
+        } else {
+            console.error("ERROR gameBoard.winCheck.gameOver ERROR");
+        }
+
+    }
+
 })();
